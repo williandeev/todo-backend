bf5023a9fdd962a5cd01ae50826f47005acc6785
Willian Robson <120076321+williansnex@users.noreply.github.com>
2023-10-18 12:38:47 -0300

    up

--- ToDoList/ToDoList/settings.py
+++ ToDoList/ToDoList/settings.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = ['todo-backend-eosin.vercel.app']
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:42090",
 ]
