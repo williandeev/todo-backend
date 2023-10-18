582ab97d0dccd711b7ad6a68c6968d18ba92655c
Willian Robson <120076321+williansnex@users.noreply.github.com>
2023-10-18 12:29:59 -0300

    update

--- ToDoList/ToDoList/settings.py
+++ ToDoList/ToDoList/settings.py
@@ -123,17 +123,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200",
+    "http://localhost:42090",
 ]
 CORS_ALLOW_METHODS = [
     "GET",
