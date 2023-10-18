fcecbf37c8128460a9c2ebeeaaaa19c21e876289
Willian Robson <120076321+williansnex@users.noreply.github.com>
2023-10-18 12:36:05 -0300

    upd

--- ToDoList/ToDoList/settings.py
+++ ToDoList/ToDoList/settings.py
@@ -132,6 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+ALLOWED_HOSTS = ['todo-backend-eosin.vercel.app']
+
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:42090",
